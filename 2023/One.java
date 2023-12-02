46da5499a2d3af1b3d12b2689a15c19b974f92c1
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2023-12-02 11:36:09 -0600

    Dailies

--- 2023/One.java
+++ 2023/One.java
@@ -2,22 +2,21 @@ import java.util.Scanner;  // Import the Scanner class
 import java.util.ArrayList; // import the ArrayList class
 
 import java.util.*; // Allows Character.isDigit()
-import java.lang.*; 
+import java.lang.*;
+import java.lang.reflect.Array; 
 
 class One {
     public static void main(String[] args) {
         // Array List of Calibration Values
         ArrayList<String> calibStrings = new ArrayList<String>();
+        ArrayList<Character> calibValues = new ArrayList<Character>();
 
-        Scanner myInput = new Scanner(System.in);
-        for (int i = 0; i < 4; i++) { // Loops 4 Values
-            System.out.println("Enter calibration value: ");
-
-            String userInput = myInput.nextLine(); // Reads Line of Input
-            //System.out.println("You entered: " + userInput); // Debugging
-            calibStrings.add(userInput);
-        }
+        calibStrings.add("a2b5j1a");    // 2 1
+        calibStrings.add("a6f");        // 6 6
+        calibStrings.add("bh3j6k9a");   // 3 9
+        calibStrings.add("4n6");        // 4 6
         
+        // Start -> Finish
         for (int x = 0; x < calibStrings.size(); x++) 
         { // Loop calib values
             String tempCalib = calibStrings.get(x);
@@ -26,10 +25,33 @@ class One {
                 char letter = tempCalib.charAt(y);
                 if (Character.isDigit(letter)) 
                 {
-                    System.out.println("Number Found: " + letter);
+                    //System.out.println("Number Found: " + letter);
+                    calibValues.add(letter);
+                    break;
                 }
             }
         }
-        
+
+        for (int x = 0; x < calibStrings.size(); x++) 
+        { // Loop calib values
+            String tempCalib = calibStrings.get(x);
+            //System.out.println(tempCalib);
+            for (int y = tempCalib.length(); y == 0; y--) 
+            { // Check individual calib values
+                char letter = tempCalib.charAt(y);
+                System.out.println(letter);
+                if (Character.isDigit(letter)) 
+                {
+                    //System.out.println("Number Found: " + letter);
+                    calibValues.add(letter);
+                    break;
+                }
+            }
+        }
+
+        for (int w = 0; w < calibValues.size(); w++) 
+        {
+            System.out.println(calibValues.get(w));
+        }
     }
 }
